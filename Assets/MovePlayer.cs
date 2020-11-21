db43dd6ca6f7ce2b382ea7dc511a04c9f5a1bc51
Your Name <cosmintiby@gmail.com>
2020-11-21 17:26:39 +0200

    primul commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float PlayerSpeed = 2f; //declaram variabila care apare in inspectorda
+    public bool doNormalize = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); //pentru tastele A si D 
+        float v = Input.GetAxis("Vertical"); //pentru tastele W si S
+
+        Vector3 dir = new Vector3(h, 0, v);
+        if (doNormalize)
+            dir = dir.normalized;
+ 
+        Vector3 offset = dir * Time.deltaTime * PlayerSpeed;
+        transform.position += offset;
+
+    }
+}
